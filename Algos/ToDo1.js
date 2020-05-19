0420b0f8396941e0af62145c6d4bf74c178f1c99
aprilchomp <aprilchomp@gmail.com>
2020-05-19 16:58:51 -0500

    ToDo1 finish

--- Algos/ToDo1.js
+++ Algos/ToDo1.js
@@ -111,51 +111,65 @@
 
 // 1. Push Front: Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
 
-// function newArr(arr, item){
-//     arr.unshift(item);
-//     return arr;
-// }
-// console.log(newArr([2, 3, 4], 1))
+function newArr(arr, item){
+    arr.unshift(item);
+    return arr;
+}
+console.log(newArr([2, 3, 4], 1))
 
 // 2. Pop Front: Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
 
-// function popFront(arr) {
-//     let temp = arr[arr.length-1];
-//     arr[arr.length-1] = arr[0];
-//     arr[0] = temp;
-//     return arr.pop()
-// }
-// console.log(popFront([1, 2, 3]))
+function popFront(arr) {
+    let temp = arr[arr.length-1];
+    arr[arr.length-1] = arr[0];
+    arr[0] = temp;
+    return arr.pop()
+}
+console.log(popFront([1, 2, 3]))
 
 // 3. Insert At: Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
 
-// function insertAt(arr, ind, val) {
-//     arr[arr.length] = val;
-//     for (var x = arr.length-1; x > ind; x--) {
-//         var temp = arr[x];
-//         arr[x] = arr[x-1];
-//         arr[x-1] = temp;
-//     }
-//     return arr;
-// }
-// console.log(insertAt([1, 2, 3, 4], 1, 6))
+function insertAt(arr, ind, val) {
+    arr[arr.length] = val;
+    for (var x = arr.length-1; x > ind; x--) {
+        var temp = arr[x];
+        arr[x] = arr[x-1];
+        arr[x-1] = temp;
+    }
+    return arr;
+}
+console.log(insertAt([1, 2, 3, 4], 1, 6))
 
 // 4. Remove At: Given an array and an index into array, remove and return the array value at that index. Do this without using built-in array methods except pop(). Think of popFront(arr) as equivalent to removeAt(arr,0).
 
-// function removeAt(arr, ind){
-//     let temp = arr[arr.length-1];
-//     arr[arr.length-1] = arr[ind];
-//     arr[ind] = temp;
-//     return arr.pop()
-// }
-// console.log(removeAt([2,3,4,5], 1))
+function removeAt(arr, ind){
+    let temp = arr[arr.length-1];
+    arr[arr.length-1] = arr[ind];
+    arr[ind] = temp;
+    return arr.pop()
+}
+console.log(removeAt([2,3,4,5], 1))
 
 // 5. Swap Pairs: Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, change input ["Brendan",true,42] to [true,"Brendan",42]. As with all array challenges, do this without using any built-in array methods.
 
+function swap(arr){
+    if(arr.length%2 == 0){
+        for(var x = 0; x < arr.length; x += 2){
+            var temp = arr[x];
+            arr[x] = arr[x+1];
+            arr[x+1] = temp;
+        }
+    }
+    else {
+        for(var x = 0; arr.length-1; x += 2){
+            var temp = arr[x];
+            arr[x] = arr[x+1];
+            arr[x+1] = temp;
+        }
+    }
+    return arr;
+}
 
-
-
-
-
+console.log(swap([1,2,3,4]));
 
 // 6. Remove Duplicates: Sara is looking to hire an awesome web developer and has received applications from various sources. Her assistant alphabetized them but noticed some duplicates. Given a sorted array, remove duplicate values. Because array elements are already in order, all duplicate values will be grouped together. As with all these array challenges, do this without using any built-in array methods. -- Second: Solve this without using any nested loops.
