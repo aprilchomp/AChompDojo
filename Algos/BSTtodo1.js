e21b1522d0760a2547484da3a62482b20d2e1117
aprilchomp <aprilchomp@gmail.com>
2020-05-29 16:01:40 -0500

    BSTtodo1

---
+++ Algos/BSTtodo1.js
@@ -0,0 +1,140 @@
+// BST: Add: // Create an add(val) method on the BST object to add new value to the tree. This entails creating a BTNode with this value and connecting it at the appropriate place in the tree. Unless specified otherwise, BSTs can contain duplicate values.
+
+
+// BST: Contains: // Create a contains(val) method on BST that returns whether the tree contains a given value. Take advantage of the BST structure to make this a much more rapid operation than SList.contains() would be.
+
+
+// BST: Min: // Create a min() method on the BST class that returns the smallest value found in the BST.
+
+
+// BST: Max: // Create a max() BST method that returns the largest value contained in the binary search tree.
+
+
+// BST: Size: // Write a size() method that returns the number of nodes (values) contained in the tree.
+
+
+// BST: Is Empty: // Create an isEmpty() method to return whether the BST is empty (whether it contains no values).
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    // BST: ADD
+    add(value) {
+        if(this.root) {
+            var runner = this.root;
+            while(runner) {
+                if(value>runner.value) {
+                    if(runner.right){
+                        runner = runner.right;
+                    } else {
+                        runner.right = new Node(value);
+                        return this
+                    }
+                } else {
+                    if(runner.left){
+                        runner = runner.left;
+                    } else {
+                        runner.left = new Node(value);
+                        return this
+                    }
+                }
+            }
+        }
+        this.root = new Node(value);
+        return this
+    }
+    contains(value) {
+        var runner = this.root;
+        while (runner) {
+            if (value == runner.value) {
+                return true;
+            }
+            if (value < runner.value) {
+                if (!runner.left) {
+                    return false;
+                }
+                runner = runner.left;
+            } else {
+                if (!runner.right) {
+                    return false;
+                }
+                runner = runner.right;
+            }
+        }
+        return false;
+    }
+    min() {
+        var runner = this.root;
+        var min = this.root.value;
+        while(runner.left) {
+            if(runner.left.value < min) {
+                min = runner.left.value;
+            }
+            runner = runner.left;
+        }
+        return min
+    }
+    max() {
+        var runner = this.root;
+        var max = this.root.value;
+        while(runner.right) {
+            if(runner.right.value > max) {
+                max = runner.right.value;
+            }
+            runner = runner.right;
+        }
+        return max
+    }
+    size() {
+        if (this.root === null) {
+            return 0;
+        }
+        function sizeHelp(runner) {
+            if (!runner) {
+                return 0;
+            }
+            return 1 + sizeHelp(runner.left) + sizeHelp(runner.right);
+        }
+        return sizeHelp(this.root);
+    }
+    isEmpty() {
+        if(this.root) {
+            return false
+        }
+        return true
+    }
+}
+// creating a new BST
+tree = new BST();
+
+// adding some nodes
+tree.add(10).add(20).add(5).add(7).add(21).add(2).add(9).add(17).add(6).add(4)
+
+// searching for value
+console.log(tree.contains(17))
+// searching for value that doesn't exist in the tree
+console.log(tree.contains('hello'))
+
+// testing min
+console.log(tree.min())
+// testing max
+console.log(tree.max())
+
+// testing size
+console.log(tree.size())
+
+//testing isEmpty
+console.log(tree.isEmpty())
+emptyTree = new BST();
+console.log(emptyTree.isEmpty())
+
+// printing our tree
+console.log(tree);
